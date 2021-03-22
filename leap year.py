6af2fa904731489232dd9828393dfac22a616b45
pradhumman <81158330+pradhumman@users.noreply.github.com>
2021-03-22 15:52:18 +0530

    Add files via upload

---
+++ leap year.py
@@ -0,0 +1,12 @@
+n = int(input('enter the year'))
+if n%4 == 0:
+    if n%100 == 0:
+        if n%400 == 0:
+            print('{n} is a leap year', format(n))
+        else:
+             print ('{n} is a leap year', format(n))
+    else:
+        print('{n} is not a leap year', format(n)
+else:
+    print('{n} is a leap year', format(n))
+    
